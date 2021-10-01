8c7d8a5a2f393dd6ad27d54a0eb85d64d67264c8
duduvillar10 <duduvillar10io@hotmail.com>
2021-10-01 13:44:01 -0300

    using db

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import createCategoryController from '../modules/cars/useCases/createCategory';
 import { importCategoryController } from '../modules/cars/useCases/importCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
@@ -11,8 +11,7 @@ const upload = multer({
 });
 
 categoriesRoutes.post('/', (request, response) => {
-  console.log("reload funcionando");
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
