43880cefda1893abebfe2adfce3adc7220413dc4
duduvillar10 <duduvillar10io@hotmail.com>
2021-10-01 14:18:47 -0300

    .

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -7,8 +7,11 @@ class CreateCategoryController {
 
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
-
-    await this.createCategoryUseCase.execute({ name, description });
+    try {
+      await this.createCategoryUseCase.execute({ name, description });
+    } catch (e) {
+      return response.status(401).send(e);
+    }
 
     return response.status(201).send();
   }
