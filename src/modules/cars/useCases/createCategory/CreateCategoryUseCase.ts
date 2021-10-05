10e33c54d3d56e3bca39bab6f91e50294745b4b3
duduvillar10 <duduvillar10io@hotmail.com>
2021-10-05 14:10:15 -0300

    using tsyringe

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,12 +1,17 @@
+import { inject, injectable } from 'tsyringe';
+
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IResquest {
   name: string;
   description: string;
 }
-
+@injectable()
 class CreateCategoryUseCase {
-  constructor(private categoriesRepository: ICategoriesRepository) {}
+  constructor(
+    @inject('CategoriesRepository')
+    private categoriesRepository: ICategoriesRepository,
+  ) {}
 
   async execute({ name, description }: IResquest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(
@@ -17,7 +22,7 @@ class CreateCategoryUseCase {
       throw new Error('Category already exists!');
     }
 
-    this.categoriesRepository.create({ name, description });
+    await this.categoriesRepository.create({ name, description });
   }
 }
 
