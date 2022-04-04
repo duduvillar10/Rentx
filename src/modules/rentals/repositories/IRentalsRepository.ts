15bf3f6f3b767c6d208c619775cd67ebfb9d2b46
duduvillar10 <duduvillar10io@hotmail.com>
2022-04-04 13:07:07 -0300

    feat: devolution rental use case

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -5,11 +5,8 @@ import { Rental } from '../infra/typeorm/entities/Rental';
 interface IRentalsRepository {
   findOpenRentalByCar(car_id: string): Promise<Rental>;
   findOpenRentalByUser(user_id: string): Promise<Rental>;
-  create({
-    user_id,
-    car_id,
-    expected_return_date,
-  }: ICreateRentalDTO): Promise<Rental>;
+  create(data: ICreateRentalDTO): Promise<Rental>;
+  findById(id: string): Promise<Rental>;
 }
 
 export { IRentalsRepository };
